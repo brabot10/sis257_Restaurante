b58c3110512bd1b3d1604764160cbb02220f837d
brabot10 <bryanpilcoloramendoza@gmail.com>
2023-11-22 23:14:41 -0400

    Pie de la página

--- frontend_restaurant/src/components/MainFooter.vue
+++ frontend_restaurant/src/components/MainFooter.vue
@@ -47,17 +47,64 @@
 
     <div class="container">
       <div class="row">
+        <div class="col-lg-6 col-12 mb-4 pb-2">
+          <h5 class="site-footer-title mb-3">Caracteristicas de QUE RICO!</h5>
+
+          <ul class="site-footer-links">
+            <li class="site-footer-link-item">
+              <a class="site-footer-link">Sabor</a>
+            </li>
+
+            <li class="site-footer-link-item">
+              <a  class="site-footer-link">Servicio</a>
+            </li>
+
+            <li class="site-footer-link-item">
+              <a  class="site-footer-link">Ambiente</a>
+            </li>
+
+            <li class="site-footer-link-item">
+              <a  class="site-footer-link">Higiene</a>
+            </li>
+
+            <li class="site-footer-link-item">
+              <a  class="site-footer-link">Valor</a>
+            </li>
+
+            <li class="site-footer-link-item">
+              <a  class="site-footer-link">Consistencia</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+          <h5 class="site-footer-title mb-3">¿Alguna pregunta?</h5>
+
+          <p class="text-white d-flex mb-1">
+            <a href="tel: 090-080-0760" class="site-footer-link"> +591 68613999 </a>
+          </p>
+
+          <p class="text-white d-flex">
+            <a href="mailto:hello@company.com" class="site-footer-link"> sis257@company.com </a>
+          </p>
+        </div>
 
         <div class="col-lg-3 col-md-6 col-11 mb-4 mb-lg-0 mb-md-0">
-          <h5 class="site-footer-title mb-3">Ubicacion</h5>
+          <h5 class="site-footer-title mb-3">Ubicación</h5>
 
           <p class="text-white d-flex mt-3 mb-2">
-            Sucre-Bolivia
+            Sucre=Bolivia
           </p>
         </div>
       </div>
     </div>
-
-    
+    <div class="site-footer-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-12 mt-5">
+            <p class="copyright-text">Copyright © 2023 Bryan & Daniela</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
